e9456337067816266358b38b05de0f2ab7af2b8d
naka44ski <100364192+naka44ski@users.noreply.github.com>
2022-02-27 13:52:27 +0900

    Add files via upload

--- display.js
+++ display.js
@@ -380,7 +380,7 @@ function calc() {
   var tmp2 = [];
   var tmp2_t = [];
   var tmp3 = [];
-  var tmp3_t = []
+  var tmp3_t = [];
   var tmp33 = [];
   var tmp33_t = [];
   rep = [];
@@ -392,10 +392,9 @@ function calc() {
   speed3 = [];
   time = []
 
-  console.log(window.record1,window.record2,window.centergx);
 
   for (var i = 0;i < window.record1.length; i++) {
-    var tmp = [2]; 
+    var tmp = [2];
     tmp = window.record1[i].split(',');
     tmp1_t[i] = (Number(tmp[1]));
     tmp1[i] = (Number(tmp[0]));
@@ -415,11 +414,18 @@ function calc() {
     tmp3[i] = (Number(tmp[0]));
   }
 
-  console.log(tmp1_t,tmp1,tmp2_t,tmp2,tmp3_t,tmp3);
+  for (var i = 0;5*i <tmp3.length; i++) {
+    var sum = 0;
+    for(var j = 0;j < 5; j++) {
+      sum += tmp3[5*i+j];
+    }
+    tmp33[i] = sum/5.0;
+    tmp33_t[i] = tmp3_t[5*i+2];
+  }
 
   var n1 = tmp1_t[0];
   var n2 = tmp2_t[0];
-  var n3 = tmp3_t[0];
+  var n3 = tmp33_t[0];
 
   var min = n1;
   if( n2 < min ) {
@@ -431,9 +437,8 @@ function calc() {
 
   var m1 = tmp1_t[tmp1.length-1];
   var m2 = tmp2_t[tmp2.length-1];
-  var m3 = tmp3_t[tmp3.length-1];
+  var m3 = tmp33_t[tmp33.length-1];
 
-  console.log(m1, m2, m3);
   var max = m1;
   if( max < m2 ) {
     max = m2;
@@ -442,7 +447,6 @@ function calc() {
     max = m3;
   }
 
-  console.log(min, max);
   var t1 = 0;
   var t2 = 0;
   var t3 = 0;
@@ -452,46 +456,44 @@ function calc() {
     var c = 0;
     if(tmp1_t[t1] == i){
       c = 1;
-      rep[k] = tmp1_t[t1-1];
-      rep1[k] = tmp1[t1-1];
-      rep2[k] = tmp2[t2-1];
-      rep3[k] = tmp3[t3-1];
+      rep[k] = tmp1_t[t1];
+      rep1[k] = tmp1[t1];
+      rep2[k] = tmp2[t2];
+      rep3[k] = tmp33[t3];
       t1++;
       k++;
     }
     if(tmp2_t[t2] == i){
       c = 2;
-      rep[k] = tmp2_t[t2-1];
-      rep1[k] = tmp1[t1-1];
-      rep2[k] = tmp2[t2-1];
-      rep3[k] = tmp3[t3-1];
+      rep[k] = tmp2_t[t2];
+      rep1[k] = tmp1[t1];
+      rep2[k] = tmp2[t2];
+      rep3[k] = tmp33[t3];
       t2++;
       k++;
     }
-    if(tmp3_t[t3] == i){
+    if(tmp33_t[t3] == i){
       c = 3;
-      rep[k] = tmp3_t[t3-1];
-      rep1[k] = tmp1[t1-1];
-      rep2[k] = tmp2[t2-1];
-      rep3[k] = tmp3[t3-1];
+      rep[k] = tmp33_t[t3];
+      rep1[k] = tmp1[t1];
+      rep2[k] = tmp2[t2];
+      rep3[k] = tmp33[t3];
       t3++;
       k++;
     }
   }
-
-  console.log(rep,rep1,rep2,rep3);
+  console.log(rep1,rep2,rep3);
   
   speed3[0] = 0;
-  for (let i = 1; i < rep3.length; i++) {
-    if(rep3[i]-rep3[i-1] > 100 && i != 0) {
-    }
-    else if (rep3[i]-rep3[i-1] == 0 && rep3[i-1] != 0 && rep3[i+1] != 0 && i != rep3.length-1) {
-      let speed_plus1;
-      speed_plus1 = (((rep3[i]-rep3[i-1])*T_Scale)*1000)/(rep[i]-rep[i-1]);
-      speed3[i] = (speed_plus1 + speed3[i-2])/2.0;
+  for (let i = 0; i < rep3.length; i++) {
+    if(rep3[i]-rep3[i-1] > 100 && i > 2) {
+      speed3[i] = speed3[i-1];
+    }else if(rep3[i] == rep3[i-1] || rep3[i] == 0 || rep3[i-1] == 0){
+      speed3[i] = speed3[i-1];
     }
     else{
-      speed3[i] = (((rep3[i-1]-rep3[i-2])*T_Scale)*1000)/(rep[i-1]-rep[i-2]);
+      speed3[i] = (((rep3[i]-rep3[i-1])*T_Scale)*1000)/(rep[i]-rep[i-1]);
+          console.log(rep3[i]-rep3[i-1]);
     }
   }
 
@@ -519,8 +521,6 @@ function calc() {
       speed3.pop();
     }
   }
-
-  console.log(rep,w1,w2,speed3);
 
   barChartData = {
     labels: rep,
@@ -607,7 +607,6 @@ function calc() {
   //描画処理
 var display_7 = function(chartname) {
     calc();
-    console.log(barChartData);
     ctx = document.getElementById(chartname).getContext("2d");
     window.myBar = new Chart(ctx, {
         type: 'bar',
