06ff6a06bacacacf970b0a8acb315b4ffb5ce69e
naka44ski <100364192+naka44ski@users.noreply.github.com>
2022-03-11 18:01:46 +0900

    Add files via upload

--- display.js
+++ display.js
@@ -374,11 +374,14 @@ var barChartData;
 var complexChartOption;
 
 function saikiTmp(array, array_t) {
-  for(i = 0; i < array.length; i++){
-    if(array[i-1]-array[i] >= 100 && i > 0){
+  for(i = 1; ; i++){
+    if(i >= array.length){
+      break;
+    }
+    if(Math.abs(array[i-1]-array[i]) >= 100){
       array.splice(i, 1);
       array_t.splice(i,1);
-      saikiTmp(array,array_t);
+      i = i-1;
     }
   }
 
@@ -423,32 +426,6 @@ for (var i = 0;i < window.centergx.length; i++) {
   tmp33[i] = (Number(tmp[0]));
 }
 
-var n1 = tmp1_t[0];
-var n2 = tmp2_t[0];
-var n3 = tmp33_t[0];
-
-var min = n1;
-if( n2 < min ) {
-  min = n2;
-}
-if( n3 < min ) {
-  min = n3;
-}
-
-var m1 = tmp1_t[tmp1.length-1];
-var m2 = tmp2_t[tmp2.length-1];
-var m3 = tmp33_t[tmp33.length-1];
-
-console.log(m1, m2, m3);
-var max = m1;
-if( max < m2 ) {
-  max = m2;
-}
-if( max < m3 ) {
-  max = m3;
-}
-
-console.log(min, max);
 var t1 = 0;
 var t2 = 0;
 var t3 = 0;
@@ -461,70 +438,114 @@ var tmp3pre_t = arrayTmp[1];
 var tmp3 = [];
 var tmp3_t = [];
 
-for (var i = 0;5*i <tmp3pre.length; i++) {
+for (var i = 0;i <tmp3pre.length-1; i++) {
   var sum = 0;
-  for(var j = 0;j < 5; j++) {
-    sum += tmp3pre[5*i+j];
-}
-tmp3[i] = sum/5.0;
-tmp3_t[i] = tmp3pre_t[5*i+2];
+  for(var j = 0;j < 8; j++) {
+    sum += tmp3pre[i+j];
+  }
+  tmp3[i] = sum/8.0;
+
+  var sum_t = 0;
+  for(var j=0; j<6; j++) {
+    sum_t += tmp3pre_t[i+j];
+  }
+  tmp3_t[i] = sum_t/6.0;
 }
 
+var n1 = tmp1_t[0];
+var n2 = tmp2_t[0];
+var n3 = tmp3_t[0];
+
+var min = n1;
+if( n2 < min ) {
+  min = n2;
+}
+if( n3 < min ) {
+  min = n3;
+}
+
+var m1 = tmp1_t[tmp1.length-1];
+var m2 = tmp2_t[tmp2.length-1];
+var m3 = tmp3_t[tmp3.length-1];
+
+console.log(m1, m2, m3);
+var max = m1;
+if( max < m2 ) {
+  max = m2;
+}
+if( max < m3 ) {
+  max = m3;
+}
+
+console.log(min, max);
+
+const s = T_Scale*1000;
 speed3[0] = 0;
 for (let i = 1; i < tmp3.length; i++) {
   if(tmp3[i] <= tmp3[i-1]){
-    speed3[i] = 0;
-  } else speed3[i] = (((tmp3[i]-tmp3[i-1])*T_Scale)*1000)/(tmp3_t[i]-tmp3_t[i-1]);
+    speed3[i] = speed3[i-1];
+  } else {
+    speed3[i] = ((tmp3[i]-tmp3[i-1])*s)/((tmp3_t[i]-tmp3_t[i-1]));
+  }
+}
+
+for (var i = 0;i <speed3.length-1; i++) {
+  var sum = 0;
+  for(var j = 0;j < 8; j++) {
+    sum += speed3[i+j];
+  }
+  speed4[i] = sum/8.0;
 }
 
 let k = 0;
 for (let i = min; i <= max; i++){
   var c = 0;
-  if(tmp1_t[t1] == i){
+  if(tmp1_t[t1] <= i){
     c = 1;
     rep[k] = tmp1_t[t1];
     rep1[k] = tmp1[t1];
     rep2[k] = tmp2[t2];
-    rep3[k] = speed3[t3];
+    rep3[k] = speed4[t3];
     t1++;
-    k++;
-    console.log(rep[k],rep1[k],rep2[k],rep3[k]);
   }
-  if(tmp2_t[t2] == i){
+  if(tmp2_t[t2] <= i){
     c = 2;
     rep[k] = tmp2_t[t2];
     rep1[k] = tmp1[t1];
     rep2[k] = tmp2[t2];
-    rep3[k] = speed3[t3];
+    rep3[k] = speed4[t3];
     t2++;
-    k++;
-    console.log(rep[k],rep1[k],rep2[k],rep3[k]);
   }
-  if(tmp3_t[t3] == i){
+  if(tmp3_t[t3] <= i){
     c = 3;
     rep[k] = tmp3_t[t3];
     rep1[k] = tmp1[t1];
     rep2[k] = tmp2[t2];
-    rep3[k] = speed3[t3];
+    rep3[k] = speed4[t3];
     t3++;
+  }
+  if(c != 0){
     k++;
-    console.log(rep[k],rep1[k],rep2[k],rep3[k]);
   }
 }
 
-console.log(rep,rep1,rep2,rep3);
-
+let jiku = document.getElementById("jiku");
+let humikomi = document.getElementById("humikomi");
+console.log(jiku.value,humikomi.value);
+let JK = (isNaN(jiku.value) ? 1.7 : jiku.value);
+let HK = (isNaN(humikomi.value) ? 1.7 : humikomi.value);
+console.log(JK,HK);
 for (let i = 1; i < rep1.length; i++) {
-  w1[i] = 1.7*(157.63514*Math.exp(-0.00454899421*(rep1[i]-567.574356)) - 17.8917946);
+  w1[i] = JK*(157.63514*Math.exp(-0.00454899421*(rep1[i]-567.574356)) - 17.8917946);
 }
 
 for (let i = 1; i < rep2.length; i++) {
-  w2[i] = 1.7*(157.63514*Math.exp(-0.00454899421*(rep2[i]-567.574356)) - 17.8917946);
+  w2[i] = HK*(157.63514*Math.exp(-0.00454899421*(rep2[i]-567.574356)) - 17.8917946);
 }
 
-/*let del = 0;
+let del = 0;
 for(let i = rep.length-1; i >= 0; i--) {
-  if(w1[i] < 10 && w2[i] < 10){
+  if((w1[i] < 10 && w2[i] < 10) || (isNaN(w1[i]) && w2[i] < 10) || (w1[i] < 10 && isNaN(w2[i])) || (isNaN(w1[i]) && isNaN(w2[i])) ){
     del++;
   }else {
     break;
@@ -536,7 +557,23 @@ for(let i = 0; i < del; i++){
   w1.pop();
   w2.pop();
   rep3.pop();
-}*/
+}
+
+del = 0;
+for(let i = 0; i <rep.length; i++) {
+  if((w1[i] < 10 && w2[i] < 10) || (isNaN(w1[i]) && w2[i] < 10) || (w1[i] < 10 && isNaN(w2[i])) || (isNaN(w1[i]) && isNaN(w2[i])) ){
+    del++;
+  }else {
+    break;
+  }
+}
+
+for(let i = 0; i < del; i++){
+  rep.shift();
+  w1.shift();
+  w2.shift();
+  rep3.shift();
+}
 
 
 let maxSpeedDiv = document.getElementById("maxspeed");
